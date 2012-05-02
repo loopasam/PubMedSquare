3ac8f61de6ee8e7c10defec00b1c25c505787bd8
Samuel Croset <samuel.croset@gmail.com>
2012-05-02 19:14:36 +0100

    show/hide abstract

--- PubMedSquare/Article.js
+++ PubMedSquare/Article.js
@@ -18,40 +18,60 @@ function Article() {
 
 Article.prototype.registerClick = function($container){
 	var that = this.element;
-	
-	//TODO doucle click to close + alt text
+
+	//TODO alt text double click close
 	this.element.click(function(){
-		
+
 		if(that.hasClass("small")){
+			
+			//TODO appliquer deux fois la reduction (le moins et plus font merder)
+			//sinon cacher l'abstract quoi qu'il en soit
+			var factor = 2;
+			if(that.hasClass("extended")){
+				factor = 4;
+			}
+			
 			that.removeClass("small");
 			that.addClass("big");
 			var width = that.css('width');
 			width = width.substring(0, width.length - 2);
 			var height = that.css('height');
 			height = height.substring(0, height.length - 2);
-			var newHeight = height*2+16;
-			var newWidth = width*2+16;
+			var newHeight = height*factor+16;
+			var newWidth = width*factor+16;
 			that.css('height', newHeight + "px");
 			that.css('width', newWidth + "px");
 			that.children().show();
+			
+			if(!that.hasClass("extended")){
+				that.find('.abstract-text').hide();
+			}
+			
 			that.find('.text-title-article').hide();
 
 			$("#container").isotope( 'reLayout');
 		}
-		
+
 	});
-	
+
 	this.element.dblclick(function(){
+
 		
 		if(that.hasClass("big")){
+			
+			var factor = 2;
+			if(that.hasClass("extended")){
+				factor = 4;
+			}
+			
 			that.removeClass("big");
 			that.addClass("small");
 			var width = that.css('width');
 			width = width.substring(0, width.length - 2);
 			var height = that.css('height');
 			height = height.substring(0, height.length - 2);
-			var newHeight = (height-16)/2;
-			var newWidth = (width-16)/2;
+			var newHeight = (height-16)/factor;
+			var newWidth = (width-16)/factor;
 			that.css('height', newHeight + "px");
 			that.css('width', newWidth + "px");
 
@@ -60,6 +80,8 @@ Article.prototype.registerClick = function($container){
 
 			$("#container").isotope( 'reLayout');
 		}
+
+		
 		
 
 	});
@@ -84,14 +106,14 @@ Article.prototype.render = function($container){
 		this.element.addClass("review");
 	}
 	$('#container').isotope( 'insert', this.element );
-	
+
 	var fullTitle = $('<div class="full-title">'+this.title+'</div>');
 	this.element.append(fullTitle);
-	
+
 	var authorList = $('<div class="authors">'+ this.authors +'</div>');
 	this.element.append(authorList);
 
-	
+
 	var trimmedAffiliation;
 	if(this.affiliation.length > 180){
 		console.log(this.title);
@@ -110,8 +132,15 @@ Article.prototype.render = function($container){
 	var abbrevJournal = $('<div class="abbrev-journal">' + this.abbrevJournal + '</div>');
 	this.element.append(abbrevJournal);
 
-	var pmidLink = $('<div class="pmid-link"><a href="http://www.ncbi.nlm.nih.gov/pubmed/'+this.pmid+'">Get article</a>Show abstract - Close</div>');
+	var pmidLink = $('<div class="pmid-link"><a href="http://www.ncbi.nlm.nih.gov/pubmed/'+this.pmid+'">Get article</a></div>');
 	this.element.append(pmidLink);
+
+	var showAbstractButton = $('<div class="show-abstract-button">Show Abstract</div>');
+	registerAbstractButton(this.element, showAbstractButton);
+	this.element.append(showAbstractButton);
+
+	this.element.append('<div class="abstract-text">' +this.abstractText + '</div>');
+
 //	var pmid = $('<div class="pmid">'+this.pmid+'</div>');
 //	this.element.append(pmid);
 
@@ -148,3 +177,36 @@ Article.prototype.setImpact = function(impact){
 	}
 
 };
+
+//TODO problem avec la taille des box quand l'abstract est montre
+function registerAbstractButton(element, showAbstractButton){
+	showAbstractButton.toggle(function(){
+		element.find(".abstract-text").show();
+		showAbstractButton.css('background-color', 'yellow');
+		element.addClass("extended");
+		var width = element.css('width');
+		width = width.substring(0, width.length - 2);
+		var height = element.css('height');
+		height = height.substring(0, height.length - 2);
+		var newHeight = height*2+16;
+		var newWidth = width*2+16;
+		element.css('height', newHeight + "px");
+		element.css('width', newWidth + "px");
+		$("#container").isotope( 'reLayout');
+
+	}, function(){
+		showAbstractButton.css('background-color', 'blue');
+		element.find(".abstract-text").hide();
+		element.removeClass("extended");
+		var width = element.css('width');
+		width = width.substring(0, width.length - 2);
+		var height = element.css('height');
+		height = height.substring(0, height.length - 2);
+		var newHeight = (height-16)/2;
+		var newWidth = (width-16)/2;
+		element.css('height', newHeight + "px");
+		element.css('width', newWidth + "px");
+		$("#container").isotope( 'reLayout');
+
+	});
+}
